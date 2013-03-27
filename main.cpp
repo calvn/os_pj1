d963deac911c4b425486f7db912370ed41200a82
Sarah Williams <swilliams704@gmail.com>
2013-03-27 19:07:55 -0400

    Comments...

--- main.cpp
+++ main.cpp
@@ -58,17 +58,23 @@ Process::Process(int p, int t, int a_t, int pri)
 	priority = pri;
 }
 
-// 
+/*
+* Simulates running the process
+* Returns 1 if the process has burst time remaining, 0 if finished
+*/
 int Process::runProcess()
 {
+	// Set the initial wait if it had not been run before
 	if (time_remaining == process_time)
 		initial_wait = current_time - arrival_time;
+	//Decrement the time remaining if it is not finished
 	if (time_remaining > 1)
 	{
 		time_remaining--;
 		current_time ++;
 		return 1;
 	}
+	//Save and print the final wait time if the program has finished running
 	else if (time_remaining == 1)
 	{
 		time_remaining--;
@@ -81,6 +87,7 @@ int Process::runProcess()
 	return 0;
 }
 
+//returns the wait times for the process
 void Process::getWaitTimes(int & i, int& t, int& w)
 {
 	i = initial_wait;
@@ -88,6 +95,9 @@ void Process::getWaitTimes(int & i, int& t, int& w)
 	w = turnaround_time - process_time;
 }
 
+/*Resets the wait time for a process
+* Used for using the same processes for multiple algorithms
+*/
 void Process::resetWait(){
 	initial_wait = 0;
 	turnaround_time = 0;
@@ -95,28 +105,34 @@ void Process::resetWait(){
 }
 
 //-------------------------PROCESS QUEUE-----------------------------------
+/*
+* Process Queue class handles the organization of the queue of processes
+* Has functions to push processes and select processes based upon a certain
+* algorithm.
+*/
 class Process_Queue{
 	public:
-	Process_Queue(){current_index = 0;};
-	int pushProcess(Process* np);
-	int pushProcessPri(Process* np);
-	Process* pushSJFProcess(Process* np);
-	Process* pushRRProcess(Process* np);
-	void reset();
-	void outputStats();
-
-	Process* nextHighPriority();
-	Process* next();
-	Process* nextSJF();
-
-	bool isEmpty() {return (p.size() > 0); }
-
+		Process_Queue(){current_index = 0;};
+		
+		int pushProcess(Process* np);
+		int pushProcessPri(Process* np);
+		Process* pushSJFProcess(Process* np);
+		Process* pushRRProcess(Process* np);
 	
+		Process* nextHighPriority();
+		Process* next();
+		Process* nextSJF();
+		
+		void reset();
+		void outputStats();
+
 	private:
 		vector<Process*> p;
 		unsigned int current_index;
 };
 
+//Pushes a process to the back of the queue 
+//Used for FCFS
 int Process_Queue::pushProcess(Process* np)
 {
 	cout<<"[time "<<current_time<<"ms] Process "<<np->getPid();
@@ -125,6 +141,8 @@ int Process_Queue::pushProcess(Process* np)
 	return 1;  
 }
 
+//Pushes a process to the back of the queue
+//Returns if the new process is a higher priority than the current running process
 int Process_Queue::pushProcessPri(Process* np)
 {
 	cout<<"[time "<<current_time<<"ms] Process "<<np->getPid();
@@ -140,21 +158,26 @@ int Process_Queue::pushProcessPri(Process* np)
 	}
 }
 
+//Inserts a process at the end of the queue
+//The head of the queue is the current index
 Process* Process_Queue::pushRRProcess(Process* np)
 {
 	cout<<"[time "<<current_time<<"ms] Process "<<np->getPid();
         cout<<" created (requires "<<np->processTime() <<"ms CPU time)"<< endl;
 	
+	//If the queue is empty
 	if (p.size() < 1)
 	{
 		p.push_back(np);
 		current_index = 1;
 	}
+	//Insert into queue directly "behind" the head
 	else
 	{
 		int queue_end = current_index;
 		if (queue_end < 1 )
 			queue_end = p.size();
+		//Insert directly before the current position
 		p.insert(p.begin() +  queue_end - 1, np);
 		current_index++;
 	}
@@ -188,18 +211,23 @@ Process* Process_Queue::pushSJFProcess(Process* np)
 	return np;  
 }
 
+//Returns the first process after the current index that has remaining burst time
 Process* Process_Queue::next()
 {
 	if (current_index == p.size())
 		current_index = 0;
+	//Return the current_index if there is time remaining
 	if(p[current_index]->timeRemaining() > 0)
 		return p[current_index++];
 	else 
 	{
 		unsigned int temp = current_index + 1;
+		//Loop to begining if end of vector is reached
 		if (temp == p.size())
 			temp = 0;
+		//Loop through all of the indexes once
 		while(temp != current_index){
+			//Return if there is time remaining
 			if (p[temp]->timeRemaining() > 0)
 			{
 				current_index = temp;
@@ -210,16 +238,20 @@ Process* Process_Queue::next()
 				temp = 0;
 		}
 	}	
+	//Return null if no processes with time remaining was found
 	return NULL;	
 }
 
+//Returns the next process with the highest priority
 Process* Process_Queue::nextHighPriority()
 {
+	//Loop if end of vector was reached
 	if (current_index == p.size())
 		current_index = 0;
 	
 	int highest_priority = MAX_PRIORITY + 1;
 
+	//Determine the highest priority in the queue
 	for (unsigned int i = 0; i < p.size(); i++)
 	{
 		if (highest_priority > p[i]->getPriority() && p[i]->timeRemaining()>0)
@@ -227,21 +259,23 @@ Process* Process_Queue::nextHighPriority()
 	}
 	unsigned int loc = current_index;
 	unsigned int n = 0;
+	//Loop through the queue to find the first with the highest priority
 	while(n < p.size())
 	{	
 		if(p[loc]->timeRemaining() > 0)
 		{
+			//Return the first with the highest priority
 			if (p[loc]->getPriority() == highest_priority)
 			{
 				current_index = loc;
 				return p[current_index++];
 			}
 		}
-
 		n++;
 		if (++loc == p.size())
 			loc = 0;
 	}	
+	//If none were found, return null
 	return NULL;	
 	
 }
@@ -257,6 +291,7 @@ Process* Process_Queue::nextSJF()
 	return NULL;
 }
 
+//Outputs the statistics of the queue
 void Process_Queue::outputStats()
 {
 	int init, turn, wait;
@@ -264,6 +299,7 @@ void Process_Queue::outputStats()
 	int imin = MAX_BURST*NUM_PROCESSES, isum = 0, imax = 0;
 	int wmin = MAX_BURST*NUM_PROCESSES, wsum = 0, wmax = 0;
 	
+	//Loop through all processes and calculate the wait times for each
 	for (unsigned int i = 0; i < p.size(); i++){
 		p[i]->getWaitTimes(init, turn, wait);
 		if (turn < tmin) tmin = turn;
@@ -286,7 +322,7 @@ void Process_Queue::outputStats()
 	cout<<"Total wait time: min "<<wmin<<"ms; avg "<<wsum/p.size()<<"ms; max "<<wmax<<"ms"<<endl;
 }	
 
-
+//resets all of the processes to be reused for a different queue
 void Process_Queue::reset()
 {
 	for (unsigned int i = 0; i < p.size(); i++)
@@ -297,11 +333,16 @@ void Process_Queue::reset()
 
 
 //-------------------------------ALGORITHMS-----------------------------
+/*
+*Function that performs the First Come First Served algorithm
+*/
 int FCFS(Process** p){
 	current_time = 0;
 	cout<<"FCFS"<<endl;
+	//Initialize the queue
 	Process_Queue pq;
 	int p_index;
+	//Push processes that start at time 0
 	for (p_index = 0; p_index < NUM_PROCESSES; p_index++)
 	{
 		if (p[p_index]->getArrival() == 0)
@@ -311,33 +352,44 @@ int FCFS(Process** p){
 		else 
 			break;	
 	}
+	
 	int prev_process = 0;
+	//Get the next process in the queue
 	Process* current_p = pq.next();
+	//While there is a next process
 	while(current_p != NULL)
 	{
+		//Run for all processes while there is a process in the ready queue
 		for (int i = 0; i < NUM_PROCESSES && current_p != NULL; i++)
 		{
 			int ran = 1;
-			for (int j = 0; j < 4000 && ran==1; j++)
+			//Run the process until the process has finished
+			for (int j = 0; j < MAX_BURST && ran==1; j++)
 			{
 				ran = current_p->runProcess();
+				//If a process arrives, add it to the queue
 				if (p_index < NUM_PROCESSES && current_time == p[p_index]->getArrival())
 					pq.pushProcess(p[p_index++]);
 			}
+			//Get the next process
 			prev_process = current_p->getPid();
 			current_p = pq.next();
+			//If it is valid and different, context switch
 			if (current_p != NULL && current_p->getPid() != prev_process)
 			{
 				cout<<"[time "<<current_time<<"ms] Context switch (swapping out process "<<prev_process<<" for process "<<current_p->getPid()<<")"<<endl;
 				int temp = current_time + tcs;
+				//Check that no processes have arrived
 				for (; p_index < NUM_PROCESSES && current_time < temp; current_time++)
 				{
 					if (p[p_index]->getArrival()== current_time)
 						pq.pushProcess(p[p_index++]);
 				}
+				//ensure the current time is accurate
 				current_time = temp;
 			}
 		}
+		//If the CPU is idle and there are still processes, push the next process to the queue
 		if (p_index < NUM_PROCESSES)
 		{
 			int next_arrival = p[p_index]->getArrival();
@@ -346,25 +398,31 @@ int FCFS(Process** p){
 			current_p = pq.next();
 		}
 	}
+	//Output the statistics
 	pq.outputStats();
 	pq.reset();
 	return 0;
 }
 
-
-
+/*
+* Executes the Round Robin Algorithm on a set of processes
+*/
 int RR(Process** p){
 	current_time = 0;
-	cout<<"ROUND ROBIN"<<endl;	
-	 Process_Queue pq;
+	cout<<"ROUND ROBIN"<<endl;
+	//Initialize the queue	
+	Process_Queue pq;
 	int p_index;
 	int next_arrival = 0;
+	
+	//Push all processes that start at time 0
 	for (p_index = 0; p_index < NUM_PROCESSES; p_index++)
 	{
 		if (p[p_index]->getArrival() == 0)
 		{	
 			pq.pushRRProcess(p[p_index]);
 		}
+		//If it does not start at time 0, save the arrival time for reference and continue
 		else 
 		{
 			next_arrival = p[p_index]->getArrival();
@@ -372,13 +430,18 @@ int RR(Process** p){
 		}	
 	}
 	int prev_process = 0;
+	//Get the first process in the RR queue
 	Process* current_p = pq.next();
+
+	//While there is a process in the ready queue
 	while(current_p != NULL)
 	{
 		int ran = 1;
+		//Run the process for the time slice or until completion
 		for (int i = 0 ; i < TIME_SLICE && ran == 1; i++)
 		{
 			ran = current_p->runProcess();
+			//If a process arrives, push it to the queue
 			if (current_time == next_arrival)
 			{
 				pq.pushRRProcess(p[p_index++]);
@@ -386,13 +449,16 @@ int RR(Process** p){
 					next_arrival = p[p_index]->getArrival();
 			}
 		}
+		//Get the next process
 		prev_process = current_p->getPid();
 		current_p = pq.next();
+		//If it is different and not null, perform a context switch
 		if (current_p != NULL && current_p->getPid() != prev_process){
 			cout<<"[time "<<current_time<<"ms] Context switch (swapping out process "<<prev_process<<" for process "<<current_p->getPid()<<")"<<endl;
 			int temp = current_time + tcs;
 			for(;p_index < NUM_PROCESSES && current_time < temp; current_time++)
 			{
+				//If a process arrives during context switch, push it to the queue
 				if (current_time == next_arrival)
 				{
 					pq.pushRRProcess(p[p_index++]);
@@ -402,6 +468,8 @@ int RR(Process** p){
 			}	
 			current_time = temp;
 		}
+		//If the CPU is idle (no current process) and there are still processes to run
+		//	jump to next arrival time
 		if (current_p == NULL && p_index < NUM_PROCESSES){
 			current_time = next_arrival;
 			pq.pushRRProcess(p[p_index++]);
@@ -410,6 +478,7 @@ int RR(Process** p){
 				next_arrival = p[p_index]->getArrival();
 		}
 	}
+	//Output the statistics and reset the variables of each process
 	pq.outputStats();
 	pq.reset();
 	return 0;
@@ -466,11 +535,10 @@ int SJF(Process** p){
 			}
 			current_time = temp;
 	    }
-	    if (current_p == NULL && p_index < NUM_PROCESSES){
-			int next_arrival = p[p_index]->getArrival();
-			current_time = next_arrival;
-			pq.pushProcess(p[p_index++]);
-			current_p = pq.next();
+	    if (p_index < NUM_PROCESSES && current_p == NULL){
+		current_time = p[p_index]->getArrival();
+		pq.pushSJFProcess(p[p_index++]);
+		current_p = pq.next();
 		}
 	}
 
@@ -556,7 +624,7 @@ int PreemptiveSJF(Process** p){
 	    }
 	    if (p_index < NUM_PROCESSES && current_p == NULL){
 			current_time = next_arrival;
-			pq.pushProcess(p[p_index++]);
+			pq.pushSJFProcess(p[p_index++]);
 			current_p = pq.next();
 			if (p_index < NUM_PROCESSES) next_arrival = p[p_index]->getArrival();
 	}
@@ -568,13 +636,18 @@ int PreemptiveSJF(Process** p){
 	return 0;
 }
 
+/*
+* Runs the preemptive priority algorithm on a set of processes
+*/
 void PreemptivePriority(Process** p)
 {
 	current_time = 0;
 	cout<<"Preemptive Priority"<<endl;
+	//Initializes the queue
 	Process_Queue pq;
 	int p_index;
 	int next_arrival;
+	//Add all processes that start at time 0
 	for (p_index = 0; p_index < NUM_PROCESSES; p_index++)
 	{
 		if (p[p_index]->getArrival() == 0)
@@ -588,27 +661,39 @@ void PreemptivePriority(Process** p)
 		}
 	}
 	int prev_process = 0;
+	// Get the first process
 	Process* current_p = pq.nextHighPriority();
 	int preempt = 0;
+	//While there are processes to run
 	while(current_p != NULL)
         {
 		int ran = 1;
+		//Run for the time slice, unless the system has been preempted
 		for (int i = 0 ; i < TIME_SLICE && ran == 1 && !preempt; i++)
 		{
 			ran = current_p->runProcess();
+			//Check if another process has arrived
 			if (current_time == next_arrival)
 			{
+				//If the process is a higher priority than the others, preempt
 				preempt = pq.pushProcessPri(p[p_index++]);
 				if (p_index != NUM_PROCESSES)
 					next_arrival = p[p_index]->getArrival();
 			}
 		}
+		//Get the next process
 		prev_process = current_p->getPid();
 		current_p = pq.nextHighPriority();
+		//Reset the preeempt flag
 		preempt = 0;
+		//If the next process is different, context switch
 		if (current_p != NULL && current_p->getPid() != prev_process){
 			cout<<"[time "<<current_time<<"ms] Context switch (swapping out process "<<prev_process<<" for process "<<current_p->getPid()<<")"<<endl;
 			int temp = current_time + tcs;
+			/*
+			* If a process has come in, add it to the queue, save if it should be preempted
+			* The process that preeempted the other will be switched in immediately after the context switch
+			*/
 			for(;p_index < NUM_PROCESSES && current_time < temp; current_time++)
 			{
 				if (current_time == next_arrival)
@@ -628,7 +713,7 @@ void PreemptivePriority(Process** p)
 			next_arrival = p[p_index]->getArrival();
 		}
 	}
-
+	//Output the wait min,avg,max wait times for the algorithm
 	pq.outputStats();
 	pq.reset();
 }
@@ -676,6 +761,7 @@ int main()
 
 	FCFS(p);
 	cout<<endl;
+
 	RR(p);
 	cout<<endl;
 
